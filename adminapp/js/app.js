736c7d38d46e08335bc0eb71c1859e80f69000af
Frank <616180981@qq.com>
2017-02-17 14:57:01 +0800

    修改了合并后的冲突

--- adminapp/js/app.js
+++ adminapp/js/app.js
@@ -1,3 +1,20 @@
 /**
  * Created by Administrator on 2017/2/17.
  */
+var routerApp = angular.module('routerApp', ['ui.router', 'oc.lazyLoad','ngCookies','ngAnimate', 'ngSanitize', 'ui.bootstrap',
+    'mainDirectives','mainCtrl','mainConstant','mainServices','mainFil']);
+
+routerApp.config(function ($statepProvider,$urlRouterProvider,$locationProvider) {
+    var _lazyLoad = function (loaded) {
+        return function ($ocLazyLoad) {
+            return $ocLazyLoad.load(loaded,{serie:true})
+        }
+    };
+    /*当没有输入跳转页面的时候，自动跳转login页面*/
+    $urlRouterProvider.when("","/login");
+    $statepProvider
+        .state('app',{
+            url:'',
+            templateUrl: ''
+        })
+})
