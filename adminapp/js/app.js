4b6613b2256972182e14253480dc9bf919a764c3
Frank <616180981@qq.com>
2017-02-17 23:23:49 +0800

    finish companyList&build

--- adminapp/js/app.js
+++ adminapp/js/app.js
@@ -1,37 +1,50 @@
 /**
  * Created by Administrator on 2017/2/17.
  */
-var adminApp = angular.module('adminApp', ['ui.router', 'oc.lazyLoad','ngCookies','ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+var adminApp = angular.module('adminApp', ['ui.router', 'oc.lazyLoad', 'ngCookies', 'ngAnimate', 'ngSanitize', 'ui.bootstrap','mainServices']);
 
 adminApp.config(['$stateProvider', '$urlRouterProvider',
-    function ($statepProvider,$urlRouterProvider) {
+    function ($statepProvider, $urlRouterProvider) {
         var _lazyLoad = function (loaded) {
             return function ($ocLazyLoad) {
-                return $ocLazyLoad.load(loaded,{serie:true})
+                return $ocLazyLoad.load(loaded, {serie: true})
             }
         };
         /*当没有输入跳转页面的时候，自动跳转main页面*/
-        $urlRouterProvider.when("","/app");
+        $urlRouterProvider.when("", "/app");
         $statepProvider
-            .state('app',{
-                url:'/app',
+            .state('app', {
+                url: '/app',
                 templateUrl: 'tpls/main.html',
                 controller: 'mainCtrl',
                 controllerAs: 'vm',
-                resolve : {
-                    home_file:_lazyLoad([
+                resolve: {
+                    home_file: _lazyLoad([
                         'css/main.css',
                         'js/mainCtrl.js'
                     ])
                 }
             })
+            .state('app.companyList',{
+                url: '/companyList',
+                templateUrl: 'tpls/companyList.html',
+                controller: 'companyListCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    home_file: _lazyLoad([
+                        'css/companyList.css',
+                        'js/companyListCtrl.js'
+                    ])
+                }
+            })
+
     }
 ])
     .controller('AccordionDemoCtrl', function ($scope) {
-    $scope.oneAtATime = true;
-    $scope.status = {
-        isCustomHeaderOpen: false,
-        isFirstOpen: true,
-        isFirstDisabled: false
-    };
-});
+        $scope.oneAtATime = true;
+        $scope.status = {
+            isCustomHeaderOpen: false,
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
+    });
