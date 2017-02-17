128062172981998b87cc6e4855f04bc328ec2705
ivws <853770545@qq.com>
2017-02-17 21:40:10 +0800

    前台路由，导航和页头

--- adminapp/js/homeCtrl.js
+++ adminapp/js/homeCtrl.js
@@ -1,3 +1,6 @@
 /**
  * Created by Administrator on 2017/2/17.
  */
+adminApp.constructor('mainhtmlCtrl',function ($scope) {
+    var vm = this;
+})
---
+++ adminapp/js/mainCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/2/17.
+ */
+angular.module('adminApp')
+.controller('mainCtrl',
+    function ($scope) {
+        var vm = this;
+        vm.name = "我是主要的控制器"
+    }
+)
