53206e475cd3a3b8a0541aa7f245ce691d2252f8
Frank <616180981@qq.com>
2017-02-18 09:23:23 +0800

    test

--- adminapp/js/app.js
+++ adminapp/js/app.js
@@ -40,3 +40,10 @@ adminApp.config(['$stateProvider', '$urlRouterProvider',
 
     }
 ])
+    .controller('AccordionDemoCtrl', function ($scope) {
+        $scope.oneAtATime = true;
+        $scope.status = {
+            isCustomHeaderOpen: false,
+            isFirstOpen: true,
+        };
+    });
