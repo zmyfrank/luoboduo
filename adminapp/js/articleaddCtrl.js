4026cdbf3cbc20b49e8b13815d6287c50f5cceff
ivws <853770545@qq.com>
2017-02-21 15:32:13 +0800

    article新增提交2

--- adminapp/js/articleaddCtrl.js
+++ adminapp/js/articleaddCtrl.js
@@ -4,12 +4,19 @@
 /* 新增article */
 angular.module('adminApp')
     .controller('articleaddCtrl',
-        function ($scope) {
+        function ($scope,$http,$state) {
         var vm = this;
         vm.articleadd = function (tp) {
-            vm.adddata = {
-                type:tp,
-            }
+            vm.adddata.status = tp;
+            $http({
+                method:'POST',
+                url:'/carrots-admin-ajax/a/u/article',
+                params:vm.adddata,
+            }).then(function (res) {
+                if (res.data.code == 0) {
+                    $state.go('app.article');
+                }
+            })
         }
     }
 )
