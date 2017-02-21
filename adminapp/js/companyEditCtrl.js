ec0939b2171bdd57e1c88e6c4af33a4471690175
Frank <616180981@qq.com>
2017-02-21 11:05:53 +0800

    完成了标签添加

--- adminapp/js/companyEditCtrl.js
+++ adminapp/js/companyEditCtrl.js
@@ -5,8 +5,18 @@ angular.module('adminApp')
     .controller('companyEditCtrl',function ($scope,getAdminSercive,FileUploader,joblisttype) {
         var vm = this;
         vm.industryData = joblisttype.industrytype;
-        vm.creatData = {};
-        vm.xxx = function () {
-            console.log(vm.output);
+        vm.taglist = []
+        vm.creatData = {
+            taglist:vm.taglist.join(',')
+        };
+        /*标签绑定*/
+        vm.joblisttype = joblisttype
+        vm.addTag = function () {
+            if (vm.taglist.indexOf(vm.tag)==-1) {
+                vm.taglist.push(vm.tag)
+            }
+        }
+        vm.deleteTag =function (index) {
+            vm.taglist.slice(index,1)
         }
     })
