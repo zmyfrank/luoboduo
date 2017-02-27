480eff8e88c7cfc3b36b2811dc1c1dd0774b5a76
ivws <853770545@qq.com>
2017-02-27 20:40:25 +0800

    生生世世

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,12 +15,13 @@ var mainCtrl = angular.module('mainCtrl', [])
             /* 底部联系我们跳转 */
             vm.aboutWe = function (bl) {
                 aboutWe.toggle = bl;
+                $cookieStore.put('pages',4);
             }
         }
     ])
     /* 关于我们页面 */
-    .controller('weCtrl', ['$scope', 'aboutWe',
-        function ($scope, aboutWe) {
+    .controller('weCtrl', ['$scope', 'aboutWe','$cookieStore',
+        function ($scope, aboutWe,$cookieStore) {
             var vm = this;
             vm.toggle = aboutWe.toggle;
 
@@ -359,7 +360,6 @@ var mainCtrl = angular.module('mainCtrl', [])
             if (res.data.code == 0) {
                 vm.jobMinute = res.data.data;
             }
-            ;
             //console.log(vm.jobMinute);
         });
     })
